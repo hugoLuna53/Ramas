2040a155ac2a4e0b4b69dcde2132268a9067deff
Hugo Luna Muñoz <hugolunamu@gmail.com>
2026-02-02 14:23:29 +0100

    Nos situamos en otra rama

---
+++ src/controller/Clase.java
@@ -0,0 +1,19 @@
+package controller;
+
+public class Clase {
+    private String ListaDeAlumnos;
+
+
+    public void agregar(){
+
+    }
+
+    public void eliminar(){
+
+    }
+
+    public void buscar(){
+
+    }
+
+}
